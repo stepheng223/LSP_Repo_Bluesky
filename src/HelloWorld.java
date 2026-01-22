90f98d41745d8f9d2fd6a2ab6484f5cbd30f96fe
stepheng223 <168613186+stepheng223@users.noreply.github.com>
2026-01-22 10:19:42 -0500

    Assignment 1 - Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ *Name: Steohen Ganthier
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.printf("Hello World!!");
+
+    }
+}

c534941be5a59143a3c2c12413ac73df939eb857
stepheng223 <168613186+stepheng223@users.noreply.github.com>
2026-01-22 17:13:20 -0500

    Final version for assignment 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.printf("Hello World!!");
+        System.out.println("Hello World!!");
 
     }
 }

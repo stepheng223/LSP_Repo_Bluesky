9cf702fa99d61c2b354d8cbcfeddca7ec5dc54c9
stepheng223 <168613186+stepheng223@users.noreply.github.com>
2026-01-23 11:58:42 -0500

    Final version for assignment 1
    git rebase --continue

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Name: Stephen Ganthier
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
